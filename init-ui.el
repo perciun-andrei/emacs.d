4f6d10bcba23be91f3d205cc26372dbc0f09dd95
Andrei Perciun <perciun.andrei@gmail.com>
2013-09-12 11:46:02 +0300

    adding extended configuratin of ido-mode

--- init-ui.el
+++ init-ui.el
@@ -1,4 +1,22 @@
 ;; Enabling ido-mode
-(ido-mode t)
+(require 'ido)
+(require 'ido-ubiquitous)
+(require 'flx-ido)
+
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess
+      ido-max-prospects 10
+      ido-default-file-method 'selected-window
+      ido-auto-merge-work-directories-length -1)
+(ido-mode +1)
+(ido-ubiquitous-mode +1)
+
+;; smarter fuzzy matching for ido
+(flx-ido-mode +1)
+
+;; disable ido faces to see flx highlights
+(setq ido-use-faces nil)
 
 (provide 'init-ui)
